ab9a28f8152557b0abdacb5a1451b96f68ff8ffa
burtenshaw <ben.burtenshaw@gmail.com>
2025-05-16 11:04:25 +0200

    work on data processing for inference

---
+++ wrangle_path_lookup.py
@@ -0,0 +1,85 @@
+from ast import literal_eval
+
+import json
+
+from datasets import DatasetDict, load_dataset, Dataset
+
+# curl -X GET "https://huggingface.co/api/datasets/OpenGVLab/GUI-Odyssey?full=true" \
+#   -H "Authorization: Bearer hf_xxx" \
+#   -o /Users/ben/code/sft-vlm-gui/dataset_config.json
+
+data = json.load(open("data/dataset_config.json"))["siblings"]
+
+SCREENSHOT_PATHS = {}
+DATASET_STUB = "https://huggingface.co/datasets/OpenGVLab/GUI-Odyssey/resolve/main/"
+
+for obj in data:
+    if not obj["rfilename"].startswith("screenshot"):
+        continue
+    path = obj["rfilename"]
+    url = f"{DATASET_STUB}{path}"
+    name = path.split("/")[-1]
+    SCREENSHOT_PATHS[name] = url
+
+SYSTEM_PROMPT = """YOU ARE JUST GREAT"""
+
+def path_to_url(path: str, idx: int = 0) -> str:
+    """
+    Convert a local file path to a URL.
+    """
+    return SCREENSHOT_PATHS.get(path, None)
+
+
+def prepare_interleaved_dataset(dataset: Dataset, max_samples: int = 10) -> Dataset:
+    """
+    Prepare the dataset for training by interleaving the images and text.
+    """
+
+    wrangled_samples = []
+
+    for n, sample in enumerate(dataset):
+        if n >= max_samples:
+            break
+        sample_messages = []
+        steps = literal_eval(sample["steps"])
+        if not isinstance(steps, list):
+            print(f"Steps is not a list for sample {n}. Skipping...")
+            continue
+        for step in steps:
+            image_path = step["screenshot"]
+            image_url = path_to_url(image_path)
+            if image_url is None:
+                print(f"Image URL not found for {image_path}. Skipping...")
+                continue
+            step_string = json.dumps(step)
+            sample_messages.append(
+                {
+                    "type": "image",
+                    "text": image_url,
+                }
+            )
+            sample_messages.append(
+                {
+                    "type": "text",
+                    "text": step_string,
+                }
+            )
+        messages = [
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT,
+            },
+            {
+                "role": "user",
+                "content": sample_messages,
+            }
+        ]
+        new_sample = {**dict(sample)}
+        new_sample["messages"] = messages
+        wrangled_samples.append(sample)
+    dataset = Dataset.from_list(wrangled_samples)
+    return dataset
+
+
+dataset = load_dataset(path="OpenGVLab/GUI-Odyssey", split="all", streaming=True)
+dataset = prepare_interleaved_dataset(dataset, max_samples=10)
